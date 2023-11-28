43429798f9b1978ef1d189f9b7b046bb0664b2c2
daniel98987 <dzambrano863@gmail.com>
2023-11-28 00:51:22 -0500

    primer commit

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,16 @@
-import { ApplicationConfig } from '@angular/core';
+import {  provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
+import {ApplicationConfig} from '@angular/core';
 import { provideRouter } from '@angular/router';
 
+import { provideAnimations } from '@angular/platform-browser/animations';
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
-};
+  providers: [
+      provideRouter(routes),
+        provideHttpClient(withInterceptorsFromDi()),
+        provideAnimations()
+    ]
+
+
+}
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        loadChildren: () => import('./core/components/user/user.routes').then(m => m.routes),
+      }
+];
